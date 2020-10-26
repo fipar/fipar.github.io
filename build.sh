1470afcb83edf0361f5e7f381c97b122436c0e4c
Fernando Ipar <fipar@acm.org>
2020-10-26 11:18:52 -0300

    sync to 522712dd65abdd95ce2ef89bc22846d6cd1e19cb (get rid of disqus)

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --name jekyll --rm -v ~/src/fernandoipar.com:/src/ -it fipar/fernandoipar.com:v1 
--- serve.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
- docker run --name jekyll --rm -p 4000:4000 -v ~/src/fernandoipar.com:/src/ -e jekyll_command=serve -it fipar/fernandoipar.com:v1 
